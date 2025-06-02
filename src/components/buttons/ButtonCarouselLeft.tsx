cb613f8f8c3df8354071f3d7242d31ba288db042
Matheus Pontes <matheuspontes5@hotmail.com>
2025-06-01 22:42:21 -0300

    ajuste em bug do botao carrossel dos produtos

--- src/components/buttons/ButtonCarouselLeft.tsx
+++ src/components/buttons/ButtonCarouselLeft.tsx
@@ -6,7 +6,8 @@ const ButtonCarouselLeft = ({ slideAnterior, className }: buttonsProps) => {
     return (
 
         <div className={`${className}`}>
-            <button onClick={slideAnterior} className="text-black text-[30px] opacity-50 cursor-pointer">❮</button>
+            <button onClick={slideAnterior} className="text-black text-[30px] opacity-50 cursor-pointer xl:flex hidden">❮</button>
+            <button className="text-black text-[30px] opacity-50 cursor-pointer xl:hidden flex">❮</button>
         </div>
 
 
--- src/components/buttons/ButtonCarouselRight.tsx
+++ src/components/buttons/ButtonCarouselRight.tsx
@@ -6,7 +6,8 @@ const ButtonCarouselRight = ({ slideProximo, className }: buttonsProps) => {
     return (
 
         <div className={`${className}`}>
-            <button onClick={slideProximo} className="text-black text-[30px] opacity-50 cursor-pointer">❯</button>
+            <button onClick={slideProximo} className="text-black text-[30px] opacity-50 cursor-pointer xl:flex hidden">❯</button>
+            <button className="text-black text-[30px] opacity-50 cursor-pointer xl:hidden flex">❯</button>
         </div>
 
 
